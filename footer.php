7443b4c054365eefe74cc6d2345ee9beee2e82fc
David <david@webcodr.de>
2015-04-02 17:38:36 +0200

    Fix link

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <div class="container">
                 <ul class="footer__menu">
                     <li class="footer__menu__item"><a href="http://www.wordpress.org/" class="footer__menu__item__link">WordPress</a></li>
-                    <li class="footer__menu__item">Theme:&nbsp;<a href="https://www.github.org/MadCatme/mcw-wp" class="footer__menu__item__link">mcw-wp</a></li>
+                    <li class="footer__menu__item">Theme:&nbsp;<a href="https://github.com/MadCatme/mcw-wp" class="footer__menu__item__link">mcw-wp</a></li>
                 </ul>
             </div>
         </footer>
