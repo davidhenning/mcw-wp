bac4b4247fa7c4a771e51e2914fb8d9eba003f26
David <david@webcodr.de>
2015-04-04 01:00:16 +0200

    Use HTTPS safe method to get the stylesheet directory URL

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Droid+Sans+Mono" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/mcw.css">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/mcw.css">
         <meta name="viewport" content="width=device-width; initial-scale=1; user-scaleable=0; maximum-scale=1;">
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <?php wp_head(); ?>
